d0f15c41c9018624e6e23b06e3d9fa074aba9415
Oleg Kostrytsyn <okostritsyn@gmail.com>
2022-11-17 18:33:54 +0200

    Made Vampire class

--- src/main/java/ua/edu/knightandwarrior/Main.java
+++ src/main/java/ua/edu/knightandwarrior/Main.java
@@ -9,10 +9,26 @@ public class Main {
         var mike = new Warrior();
         var rog = new Rookie();
         var lancelot = new Defender();
+        var vampire  = new Vampire();
 
         System.out.println("Fight Warrior vs Defender "+Battle.fight(mike, bob));
         System.out.println("Defender "+bob.getHealth());
         System.out.println("Warrior "+mike.getHealth());
         System.out.println("Fight Defender vs Rookie "+Battle.fight(lancelot, rog));
+
+        var myArmy = new Army();
+        myArmy.addUnits(Defender::new, 2);
+        myArmy.addUnits(Vampire::new, 2);
+        myArmy.addUnits(Warrior::new, 1);
+
+        var enemyArmy = new Army();
+        enemyArmy.addUnits(Warrior::new, 2);
+        enemyArmy.addUnits(Defender::new, 2);
+        enemyArmy.addUnits(Vampire::new, 3);
+
+        System.out.println("Fight myArmy vs enemyArmy "+Battle.fight(myArmy, enemyArmy));
+
+        System.out.println("Warrior vs vampire "+Battle.fight(new Warrior(), vampire));
+        System.out.println("vampire health"+vampire.getHealth());
     }
 }
---
+++ src/main/java/ua/edu/knightandwarrior/model/Vampire.java
@@ -0,0 +1,43 @@
+package ua.edu.knightandwarrior.model;
+
+public class Vampire extends Warrior {
+    private static final int VAMPIRISM=50;
+    private static final int ATTACK=4;
+    private static final int INITHEALTH=40;
+    private int health = INITHEALTH;
+
+    @Override
+    public int getAttack() {
+        return ATTACK;
+    }
+
+    public int getVampirism() {
+        return VAMPIRISM;
+    }
+
+    @Override
+    public void attack(Warrior warrior) {
+        int healthBeforeHit = warrior.getHealth();
+        warrior.receiveDamage(getAttack());
+        int healthAfterHit = warrior.getHealth();
+
+        int vampire = (healthBeforeHit-healthAfterHit)/2;
+        if ((getHealth()+vampire)<=INITHEALTH) {
+            setHealth(getHealth() + vampire);
+        }
+    }
+
+    @Override
+    protected void receiveDamage(int attack) {
+        setHealth(getHealth() - attack);
+    }
+
+    @Override
+    public int getHealth() {
+        return health;
+    }
+
+    private void setHealth(int health) {
+        this.health = health;
+    }
+}
